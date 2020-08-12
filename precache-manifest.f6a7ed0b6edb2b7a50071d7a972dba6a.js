1293320b974146249d0e70bc303bde80e3748225
abdullah <abdullah45454@gmail.com>
2020-08-13 01:00:13 +0300

    Updates

---
+++ precache-manifest.f6a7ed0b6edb2b7a50071d7a972dba6a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ae79ff2101aad3d7c2a9a7df1ce277",
+    "url": "/virtual-pinboard/index.html"
+  },
+  {
+    "revision": "2e51d01fd57febbf0c95",
+    "url": "/virtual-pinboard/static/css/main.4e319de8.chunk.css"
+  },
+  {
+    "revision": "d7ffa60788760cff65a4",
+    "url": "/virtual-pinboard/static/js/2.9477f214.chunk.js"
+  },
+  {
+    "revision": "8b32dc95a2ee91e1b5d7d27e55fe304c",
+    "url": "/virtual-pinboard/static/js/2.9477f214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0210ddcbfb5e340c8af",
+    "url": "/virtual-pinboard/static/js/3.0c3bf7b7.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/virtual-pinboard/static/js/3.0c3bf7b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e51d01fd57febbf0c95",
+    "url": "/virtual-pinboard/static/js/main.10fd63b4.chunk.js"
+  },
+  {
+    "revision": "d77c9081c8a4069a07c7",
+    "url": "/virtual-pinboard/static/js/runtime-main.1e0862e8.js"
+  },
+  {
+    "revision": "f2d77bcb5ba68ab625719fe9612bd5a6",
+    "url": "/virtual-pinboard/static/media/dart-sound.f2d77bcb.mp3"
+  }
+]);
